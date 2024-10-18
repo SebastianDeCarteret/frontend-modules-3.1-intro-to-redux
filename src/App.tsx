2bca2d6651909a02eb40fef99b5c89c8419a8ac2
Seb <sebastian.decarteret@barnett-waddingham.co.uk>
2024-10-18 12:09:08 +0100

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Counter } from "./features/Counter";
+
+function App() {
+  return (
+    <div className="p-12">
+      <Counter />
+    </div>
+  );
+}
+
+export { App };
---
+++ src/features/Counter.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./counterSlice";
+// Write your code here
+
+function Counter() {
+  const [amount, setAmount] = useState(10);
+  const count = useSelector((state) => (state as any).counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="w-fit flex flex-col gap-2">
+      <div className="flex justify-between">
+        <div>
+          <PrettyButton
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </PrettyButton>
+          <PrettyButton
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </PrettyButton>
+        </div>
+
+        <span className="text-3xl text-green-800 mr-2">{count}</span>
+      </div>
+
+      <div className="rounded-md border-2 border-sky-700">
+        <input
+          type="number"
+          className="text-xl py-2 px-2 border-r-2 border-sky-700"
+          value={amount}
+          /* e.target.value is a string, adding `+` before it is a shorthand to coerce the string into a number */
+          onChange={(e) => setAmount(+e.target.value)}
+        />
+        <button
+          className="px-4 py-2 text-xl active:scale-95"
+          aria-label="Increment by Amount"
+          /* 💃 TODO 💃 */
+          /* onClick={} */
+        >
+          Increment by Amount
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export { Counter };
+
+interface Types {
+  children: any;
+  rest?: any;
+  onClick: any;
+}
+const PrettyButton = ({ children, ...rest }: Types) => (
+  <button
+    className="rounded-md border-2 border-sky-700 px-4 py-2 mr-2 text-xl active:scale-95"
+    {...rest}
+  >
+    {children}
+  </button>
+);
